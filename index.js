5e187ae8ed2ba099a107daaaca320bbc778fbfee
ongweny <ongwenyjoel@gmail.com>
2024-04-16 09:47:15 +0300

    complete

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Write your solution here!
+
+const cats = ["Milo", "Otis", "Garfield"];
+   
+function destructivelyAppendCat(Ralph){
+    cats.push ("Ralph");
+}
+function destructivelyPrependCat(Bob){
+    cats.unshift ("Bob");
+}
+function destructivelyRemoveLastCat(Ralph){
+    cats.pop ("Ralph")
+}
+function destructivelyRemoveFirstCat(Bob){
+    cats.shift ("Bob")
+}
+
+function appendCat(rick){
+    const newcats = [...cats, rick]
+    return newcats
+}
+function prependCat(cole){
+    const beforecat = [cole, ...cats]
+    return beforecat
+}
+function removeLastCat(){
+    const removecat = cats.slice(0, -1)
+    return removecat
+}
+function removeFirstCat(){
+    const firstcatgone = cats.slice(-2)
+    return firstcatgone
+}
